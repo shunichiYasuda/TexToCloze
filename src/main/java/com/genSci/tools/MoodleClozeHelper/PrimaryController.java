99ab3444ae95a43b70c36e65ce50ef0480226b36
shunichiYasuda <34476078+shunichiYasuda@users.noreply.github.com>
2020-05-18 15:28:54 +0900

    add processing May18

--- src/main/java/com/genSci/tools/MoodleClozeHelper/PrimaryController.java
+++ src/main/java/com/genSci/tools/MoodleClozeHelper/PrimaryController.java
@@ -30,17 +30,24 @@ public class PrimaryController {
 		str = replace(regex,str,"<ol>");
 		regex = "\\\\end\\{enumerate}";
 		str = replace(regex,str,"</ol>");
+		regex ="\\\\begin\\{description}";
+		str = replace(regex,str,"<ul>");
+		regex = "\\\\end\\{description}";
+		str = replace(regex,str,"</ul>");
 		str = str.replace("{\\toi}", "{1:NM:=1}");
 		str = str.replace("\\item", "<li>");
 		str = replace("\\[label.+?\\]",str,"");
 		str = replace("\\\\ban\\{.+?}",str,"");
 		regex = "\\\\begin\\{tabular}\\{.+?}";
-		str =replace(regex,str,"<table>");
+		str =replace(regex,str,"<table><caption><b>選択肢</b></caption>\n<tr>\n<td>");
 		regex = "\\\\end\\{tabular}";
 		str = replace(regex,str,"</table>");
+		str = str.replaceAll("\\{\\\\bf.+?選択肢}","");
+		str = str.replaceAll("\\\\", "");
     	regex = "\\$.+?\\$";
-    	str =str.replaceAll(" (\\$.+?\\$)", "\\\\($1\\\\)");
+    	str =str.replaceAll("(\\$.+?\\$)", "\\\\($1\\\\)");
     	str = str.replaceAll("\\$", "");
+    	str = str.replaceAll("&","</td><td>");
     	modArea.setText(str);
     }
     //
